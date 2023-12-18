a5570eecf6816c3b78b71ecd0ffc319760a8e864
QuentinChevall <quentin.chevallier@ipsa.fr>
2023-12-18 09:48:11 +0100

    somme fonction de 2 nb

---
+++ somme.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int a;
+int b;
+printf("Entrer premier nombre \n");
+scanf("%d", &a);
+printf("Entrer deuxieme nombre \n");
+scanf("%d", &b);
+
+printf("Le nombre est : %d",a+b);
+printf("\n");
+
+return a+b;
+}

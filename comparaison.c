4a3b46fd9ed761f441bb1e906e93512085f34967
QuentinChevall <quentin.chevallier@ipsa.fr>
2023-12-18 10:20:37 +0100

    Fonction de comparaison

---
+++ comparaison.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+
+int a;
+int b;
+printf("Entrer premier nombre \n");
+scanf("%d", &a);
+printf("Entrer deuxième nombre \n");
+scanf("%d", &a);
+
+if (a>=b)
+    {
+	printf("%d",a);
+	printf(" Est le plus grand nombre \n");
+
+    }
+else
+    {
+	printf("%d",b);
+	printf(" Est le plus grand nombre \n");
+     }
+     
+return 0;
+}

f31b42f64d7236e84ba1fd8a93f58a98cd226c8e
QuentinChevall <quentin.chevallier@ipsa.fr>
2023-12-18 10:40:38 +0100

    Demande un nombre affiche sa valeur et son adresse mémoire

---
+++ memory.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	printf("Entrez un nombre \n");
+	scanf("%d", &a);
+	printf("La valeur est : %d ",a);
+	printf("\n");
+	printf("L'adresse mémoire est : %p ",a);
+	printf("\n");
+return 0;
+}

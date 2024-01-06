a70f2b8d8f9e18350a629271fa16b3125d8279fe
Quentin Chevallier <quentin.chevallier@ipsa.fr>
2024-01-06 15:14:40 +0100

    Thread qui affiche Hello World

--- semaphore_HW.c
+++ semaphore_HW.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <semaphore.h>
 
-void *printHello(void *arg) {
+sem_t sem;
+
+void* thread_func(void* arg) {
+    sem_wait(&sem); // entering critical section
     printf("Hello World\n");
+    sem_post(&sem); // leaving critical section
     return NULL;
 }
 
 int main() {
-    pthread_t thread;
-    pthread_create(&thread, NULL, printHello, NULL);
-    pthread_join(thread, NULL);
+    pthread_t t1;
+
+    // initialize semaphore
+    sem_init(&sem, 0, 1);
+
+    // create threads
+    pthread_create(&t1, NULL, thread_func, NULL);
+
+    // wait for threads to finish
+    pthread_join(t1, NULL);
+
+    // destroy semaphore
+    sem_destroy(&sem);
+
     return 0;
 }
